1574e6902852cb182b324240215226493c761bce
LucasMedeiros <lucasmedeiros02@gmail.com>
2022-08-16 16:11:04 -0300

    Product entity, SQL script

--- backend/dscatalog/src/main/java/com/lucasmedeiros/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/lucasmedeiros/dscatalog/services/CategoryService.java
@@ -1,18 +1,21 @@
 package com.lucasmedeiros.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lucasmedeiros.dscatalog.Entities.Category;
 import com.lucasmedeiros.dscatalog.dto.CategoryDTO;
 import com.lucasmedeiros.dscatalog.repositories.CategoryRepository;
+import com.lucasmedeiros.dscatalog.services.exceptions.DatabaseException;
 import com.lucasmedeiros.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -22,9 +25,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAll(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	public CategoryDTO findById(Long id) {
@@ -41,7 +44,6 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
-	//Refatorar para findById usando Optional
 	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO dto) {
 			try {
@@ -50,7 +52,17 @@ public class CategoryService {
 				entity = repository.save(entity);
 				return new CategoryDTO(entity);
 			} catch (EntityNotFoundException e) {
-				throw new ResourceNotFoundException("Id not found" + id);
+				throw new ResourceNotFoundException("Id not found " + id);
 			}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
